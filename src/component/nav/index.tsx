00c33b12399648a1db3b501ec067bc7b515bd0e8
skysky33 <101037954+skysky33@users.noreply.github.com>
2022-06-24 18:15:19 +0900

    circle_animation_inprogress

--- src/component/nav/index.tsx
+++ src/component/nav/index.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 function Nav() {
-    return (
-      <div className="nav">
-        <div className='nav-img'>
-            <img style={{width:'28%'}} src="./Mask group.png"/> 
-            <img style={{width:'72%'}} src="./EWHA-CHAIN.png"/>
+  return (
+    <div className="nav">
+      <div className="nav-img">
+        <img style={{ width: "28%" }} src="./Mask group.png" />
+        <img style={{ width: "72%" }} src="./EWHA-CHAIN.png" />
+      </div>
+      <div className="nav-btn">
+        <div className="block">
+          <button id="btn1">ABOUT US</button>
+          <div className="dot"></div>
         </div>
-        <div className="nav-btn">
-            <button>ABOUT US</button>
-            <button>RECRUITING</button>
-            <button>MEMBERS</button>
+        <div className="block">
+          <button id="btn2">RECRUITING</button>
+          <div className="dot"></div>
+        </div>
+        <div className="block">
+          <button id="btn3">MEMBERS</button>
+          <div className="dot"></div>
         </div>
       </div>
-    );
-  }
-  
-  export default Nav;
+    </div>
+  );
+}
+
+export default Nav;
