e4727f0df94afd12858ab7dd40bea344aedbea0c
topnotchsh <dltngus00@ewhain.net>
2022-06-24 20:20:45 +0900

    220624 portfolio, slogan

--- src/component/homeContent/slogan/index.tsx
+++ src/component/homeContent/slogan/index.tsx
@@ -4,12 +4,25 @@ import './index.css';
 function Slogan() {
     return (
       <div className='slogan'>
-        <div className="square-slogan"></div>
-        <p className='slogan-1'>Where Change Begins,<br/>
-            Where chain begins,<br/>
-            We are chainers.</p>
-        <p className='slogan-2'>Where We Lead The Industry of Blockchain</p>
-        <button>Apply</button>
+        <div className='toFlex'>
+          <div className='left'>
+            <div className="square-slogan"></div>
+            <p className='slogan-1'>Where Change Begins,<br/>
+                Where chain begins,<br/>
+                We are chainers.</p>
+            <p className='slogan-2'>Where We Lead The Industry of Blockchain</p>
+            <button>Apply</button>
+          </div>
+          <div className='right'>
+            <video
+              src="/sampleVideo.mp4"
+              width="600"
+              height="350"
+              loop autoPlay
+              >
+              </video>
+          </div>
+        </div>
       </div>
     );
   }
