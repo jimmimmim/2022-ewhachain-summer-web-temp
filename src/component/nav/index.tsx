d4e947ec7b58f415aa7ae56571d2304a9fa0e46c
JIYEONKIMPROG <rlawldus0527@gmail.com>
2022-06-30 16:55:52 +0900

    move to main page using nav bar

--- src/component/nav/index.tsx
+++ src/component/nav/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./index.css";
-
+import { BrowserRouter, Route,Routes, useNavigate,Outlet,Link, Navigate } from 'react-router-dom';
 function Nav() {
+  
+  const goHome=()=>{
+    console.log("냥");
+  }
   return (
     <div className="nav">
       <div className="nav-img">
+      <Link to='/'>
+      
         <img style={{ width: "28%" }} src="./Mask group.png" />
         <img style={{ width: "72%" }} src="./EWHA-CHAIN.png" />
+      
+      </Link>
       </div>
       <div className="nav-btn">
         <div className="block">
@@ -14,11 +22,11 @@ function Nav() {
           <div className="dot"></div>
         </div>
         <div className="block">
-          <button id="btn2">RECRUITING</button>
+          <button id="btn2" >RECRUITING</button>
           <div className="dot"></div>
         </div>
         <div className="block">
-          <button id="btn3">MEMBERS</button>
+          <button id="btn3" >MEMBERS</button>
           <div className="dot"></div>
         </div>
       </div>
